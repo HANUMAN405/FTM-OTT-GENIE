61e51caffa1596d0de1b48fc2c9edc271cf8590b
HANUMAN405 <153421380+HANUMAN405@users.noreply.github.com>
2025-02-21 22:56:00 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,14 @@
 from pytube import YouTube
+import os
 
 video_url = 'https://youtu.be/-X4Ri2ctj78'
 
+cookie_path = "cookies.txt"  # Save your cookies to this file
+
 try:
-    yt = YouTube(video_url)
-    stream = yt.streams.filter(progressive=True, file_extension="mp4").first()
-    if stream:
-        stream.download()
-        print("Download completed!")
-    else:
-        print("No suitable stream found.")
+    yt = YouTube(video_url, use_oauth=True, allow_oauth_cache=True)
+    stream = yt.streams.get_highest_resolution()
+    stream.download()
+    print("Download completed!")
 except Exception as e:
     print(f"Error: {e}")
